f0bca6ed6af95535028413d67a82f21ddfb5541f
Hang <Hang@163.com>
2022-08-08 23:08:27 +0800

    增加translate文件保存格式

--- service/translate/translate_service.py
+++ service/translate/translate_service.py
@@ -1,6 +1,14 @@
+import os
 from service.redis_util import singleton
 path_url = 'C:/Users/Hang/PycharmProjects/RandomDataGenerate/model/datas/translate/'
 
+'''
+数据保存格式
+|4 byte|n byte|
+|头部|单行数据|
+头部: 单行数据长度（包含头部)
+单行数据: 原文&译文
+'''
 
 @singleton
 class TranslateService(object):
@@ -29,7 +37,7 @@ class TranslateService(object):
             if sen_count[index] < pre_total_count + pagesize:
                 pagesize = sen_count[index] - pre_total_count
             for i in range(pagesize):
-                result.append(sentenses[2 * index + 1][i] + '.')
+                result.append([sentenses[2 * index + 1][i] + '.', ''])
         return title, result
 
     def compute_sentense(self, datas: []):
@@ -65,12 +73,35 @@ class TranslateService(object):
             new_lines.append(new_lines_str)
         return new_lines
 
-    def save_translate_file(self, filename: str, origin_data, translate_data, pagesize):
-        pass
+    def save_translate_file(self, filename: str, datas: [], pageindex: int, pagesize: int):
+        '''
+
+        :param filename:
+        :param datas:
+        :param pageindex:
+        :param pagesize:
+        :return:
+        '''
+        fd = None
+        if os.path.exists(path_url + filename + '_translate.txt'):
+            fd = open(path_url + filename + '_translate.txt', '+', encoding='utf-8')
+        else:
+            fd = open(path_url + filename + '_translate.txt', 'x', encoding='utf-8')
+
+    def write_line(self, fd, line: str):
+        line_bytes = line.encode('utf-8')
+        bytes_count = len(line_bytes) + 4
+        fd.write(bytes_count.to_bytes(4, byteorder='big'))
+        fd.write(line_bytes)
 
 
 if __name__ == '__main__':
-    trans_utils = TranslateService()
-    title, result = trans_utils.get_translate_file('Timid Lucy', 1, 20)
-    print(title)
-    print(result)
+    # trans_utils = TranslateService()
+    # title, result = trans_utils.get_translate_file('Timid Lucy', 1, 20)
+    # print(title)
+    # print(result)
+    a = 'hello world'
+    b = a.encode('utf-8')
+    t = len(b) + 4
+    z = t.to_bytes(4, byteorder='big')
+    print(int.from_bytes(z, byteorder='big'))
